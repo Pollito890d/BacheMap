44ad6f9c7f333774cf9e616c778b1606d79f03db
Polltio890d <dmartinezvillalobos23@gmail.com>
2026-05-12 05:04:29 -0600

    Configuración real de Supabase Login

---
+++ config-supabase.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://muhpuwittgecylwtixku.supabase.co/rest/v1/'
+const supabaseKey = 'sb_publishable_TCj4IljMPd_r9PMn_xLmIQ_ckANVu8H'
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey)
